0d1df49a880e72d71acbbe3675affdb9be1db8e0
Barchinoy0803 <barchinoyyusupova33@gmail.com>
2025-09-17 10:27:20 +0500

    unused items removed

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,6 @@ import Layout from "../pages/layout"
 import Home from "../pages/home"
 import About from "../pages/about"
 import Projects from "../pages/projects"
-import Contact from "../pages/contact"
 
 const MainRouter = () => {
     return (
